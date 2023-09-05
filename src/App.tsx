5763b77a470228f78ea5798dbd3711f7c1d1fa17
New Panuwat <new_sbh@me.com>
2023-09-06 00:41:51 +0700

    [BDP-01] Add API service fetchData pokeAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import "./custom.css";
+import PokeTable from "./features/Pokemon/PokeTable";
 
 function App() {
   return (
-    <div className="bg-section bg-page-color text-primary-color h3-typo">
-      <h1>My React Vite App</h1>
-      <p className=" body2-typo text-body-color">Custom CSS Colors</p>
-    </div>
+    <>
+      <div className="bg-section bg-page-color text-primary-color h3-typo">
+        <div className="h2-typo ">Mini project: PokeAPI</div>
+        <PokeTable />
+      </div>
+    </>
   );
 }
 
---
+++ src/api/fetchData.ts
@@ -0,0 +1,25 @@
+// api.ts
+
+import axios, { AxiosResponse } from "axios";
+import { Pokemon } from "../interface/pokemon";
+import { ApiResponse } from "../interface/apiResponse";
+import { baseURL } from "../global";
+
+const api = axios.create({
+  baseURL,
+});
+
+// Define functions to make API requests
+export const fetchData = async (endpoint: string): Promise<Pokemon[]> => {
+  try {
+    const response: AxiosResponse<ApiResponse> = await api.get(endpoint);
+    return response.data.results;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// You can add more API functions as needed
+
+export default api;
