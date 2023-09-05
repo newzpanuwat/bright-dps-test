8fbfba3d37716c90f53e0b312690dcb30c1aa749
New Panuwat <new_sbh@me.com>
2023-09-05 23:06:48 +0700

    [BDP-01] Initial project & setup Css base colors and custom fonts styles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./custom.css";
+
+function App() {
+  return (
+    <div className="bg-section bg-page-color text-primary-color h3-typo">
+      <h1>My React Vite App</h1>
+      <p className=" body2-typo text-body-color">Custom CSS Colors</p>
+    </div>
+  );
+}
+
+export default App;
